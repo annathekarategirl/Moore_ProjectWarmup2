8360d8b7ca839d611c47881adca58124626e9054
Anna Moore <annathekarategirl@gmail.com>
2026-03-04 14:05:20 -0500

    d

--- script.js
+++ script.js
@@ -17,15 +17,15 @@ function describeRoom(room){
     }
 }
 
-const exampleNode = {image: './images/hallway.jpg', txt:"The floors of this liminal space creek with every step.",choices:["Walk further down the hall","Kick down a door for funsies","Stay where you are"]
+const exampleNode = {image: 'images/hallway.jpg', txt:"The floors of this liminal space creek with every step.",choices:["Walk further down the hall","Kick down a door for funsies","Stay where you are"]
 }
 const storyNodes=[{
-        image: '.\\images\\hallway.jpg', 
+        image: 'images/hallway.jpg', 
         txt:"The floors of this liminal space creek with every step.",
         choices:["Walk further down the hall","Kick down a door for funsies","Stay where you are"]},
 
     {
-        image: ".\\images\\bedroom.jpg",
+        image: "images/bedroom.jpg",
         txt:"A trenchcoat hangs on the wall by the bed... postal dude?",
         choices:["Wear the trenchcoat","Burn the trenchcoat","Lay in the bed", "Make the bed"]
     },
