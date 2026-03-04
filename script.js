423f01a04cae05770e28f05636682c59f4fe2aba
Anna Moore <annathekarategirl@gmail.com>
2026-03-04 13:50:33 -0500

    n

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function describeRoom(room){
-    if(room=="hallway"){
-        return "The floors of this liminal space creek with every step."
+    switch(room){
+        case "hallway":
+            return "The floors of this liminal space creek with every step."
+            break;
+        case "sink":
+            return "The sink is dirty, probably filled with lead"
+            break;
+        case "stairwell":
+            return "I wonder what's upstairs"
+            break;
+        case "bedroom":
+            return "A trenchcoat hangs on the wall by the bed... postal dude?"
+            break;
+        default:
+            return "Probably not the loveliest place to be right now..."
     }
 }
-console.log(describeRoom("hallway"))
